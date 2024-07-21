88dcc7db8384149c6a05234b0390a7507e3800fa
Yakshit Chhipa <yakshitchhipa@gmail.com>
2024-07-21 15:25:59 +0530

    feat: genric linked list node

--- ll_node.h
+++ ll_node.h
@@ -1,10 +1,15 @@
-#include <iostream> 
+#ifndef NODE_H
+#define NODE_H
 
-class Node { 
-    public: 
-        int data; 
-        Node* next; 
-        Node(int data);
+#include <iostream>
+
+template <typename T> class Node {
+public:
+    T data;
+    Node* next;
+    Node(T data);
 };
 
-// template <typename T> inline Node<T>::Node(T data) {}
+template <typename T> Node<T>::Node(T data) : data(data), next(nullptr) {}
+
+#endif
